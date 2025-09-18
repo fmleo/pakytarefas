424c0a470ce9e9b98ff9ff46cfe8d1b50bfa10f0
Leonardo Fochesato Michelon <fmleonardo@protonmail.com>
2025-09-18 19:46:53 -0300

    chore: remove unused code

--- src/main.py
+++ src/main.py
@@ -7,11 +7,3 @@ if __name__ == "__main__":
     logging.info("Iniciando aplicação")
     init_db()
     cli()
-
-    # with get_session() as session:
-    #     gincana_repo = GincanaRepository(session)
-
-    #     for g in gincana_repo.get_active_gincanas():
-    #         print(g)
-
-    # print(ScraperFactory.get_all_scrapers())
